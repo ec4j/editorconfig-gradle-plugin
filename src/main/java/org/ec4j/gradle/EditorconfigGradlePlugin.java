e9bf4770ad625c386d8779aee6670b9cd7afef97
Kamil Perczyński <kamil.perczynski.95@gmail.com>
2024-01-23 22:22:03 +0100

    fix: editorconfig-linters dependency declaration scope

--- src/main/java/org/ec4j/gradle/EditorconfigGradlePlugin.java
+++ src/main/java/org/ec4j/gradle/EditorconfigGradlePlugin.java
@@ -18,7 +18,6 @@ package org.ec4j.gradle;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.dsl.DependencyHandler;
 
 /**
  * Adds {@link EditorconfigExtension}, {@link EditorconfigCheckTask} and {@link EditorconfigFormatTask}.
@@ -27,17 +26,12 @@ import org.gradle.api.artifacts.dsl.DependencyHandler;
  */
 public class EditorconfigGradlePlugin implements Plugin<Project> {
     public static final String CONFIGURATION_NAME = "editorconfig";
-    /** The version of {@code org.ec4j.linters:editorconfig-linters} keep in sync with the version in {@code build.gradle} */
-    private static final String LINTERS_VERSION = "0.2.1";
 
     /** {@inheritDoc} */
     @Override
     public void apply(Project project) {
         project.getExtensions().create(EditorconfigExtension.NAME, EditorconfigExtension.class);
-        project.getRepositories().add(project.getRepositories().mavenCentral());
         project.getConfigurations().maybeCreate(CONFIGURATION_NAME);
-        final DependencyHandler dependencies = project.getDependencies();
-        dependencies.add(CONFIGURATION_NAME, "org.ec4j.linters:editorconfig-linters:" + LINTERS_VERSION);
 
         project.getTasks().create(EditorconfigCheckTask.NAME, EditorconfigCheckTask.class);
         project.getTasks().create(EditorconfigFormatTask.NAME, EditorconfigFormatTask.class);
